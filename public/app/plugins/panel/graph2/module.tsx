0d922af0a820c0c5a30616c498dd55e15e213727
Peter Holmberg <peter.hlmbrg@gmail.com>
2018-11-02 10:42:36 +0100

    added rc-slider

--- public/app/plugins/panel/graph2/module.tsx
+++ public/app/plugins/panel/graph2/module.tsx
@@ -1,6 +1,7 @@
 // Libraries
 import _ from 'lodash';
 import React, { PureComponent } from 'react';
+import Slider from 'rc-slider';
 
 // Components
 import Graph from 'app/viz/Graph';
@@ -9,6 +10,7 @@ import { Switch } from 'app/core/components/Switch/Switch';
 
 // Types
 import { PanelProps, NullValueMode } from 'app/types';
+import { Label } from '../../../core/components/Label/Label';
 
 interface Options {
   showBars: boolean;
@@ -40,9 +42,34 @@ export class TextOptions extends PureComponent<any> {
 
   render() {
     return (
-      <div className="section gf-form-group">
-        <h5 className="section-heading">Draw Modes</h5>
-        <Switch label="Lines" checked={true} onChange={this.onChange} />
+      <div>
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Draw Modes</h5>
+          <Switch label="Lines" checked={true} onChange={this.onChange} />
+        </div>
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Mode options</h5>
+          <div className="gf-form">
+            <Label width={5}>Fill</Label>
+            <Slider
+              min={1}
+              max={10}
+              step={1}
+              dots={true}
+              dotStyle={{
+                border: 'none',
+              }}
+              trackStyle={{ backgroundColor: 'orange', height: 2 }}
+              handleStyle={{
+                border: '1px solid red',
+                height: 15,
+                width: 15,
+                backgroundColor: 'white',
+              }}
+              railStyle={{ backgroundColor: 'white', height: 2 }}
+            />
+          </div>
+        </div>
       </div>
     );
   }
