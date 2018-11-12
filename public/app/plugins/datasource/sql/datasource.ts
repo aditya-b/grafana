0f4dcbe527ae180219204bb8e5e214214d8336d9
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2018-11-12 11:28:00 +0100

    start extracting common sql functionality

---
+++ public/app/plugins/datasource/sql/datasource.ts
@@ -0,0 +1,143 @@
+import SqlQuery from './query';
+import ResponseParser from './response_parser';
+
+export abstract class SqlDatasource {
+  id: any;
+  name: any;
+  interval: string;
+  queryModel: SqlQuery;
+  responseParser: ResponseParser;
+
+  constructor(instanceSettings, private backendSrv, private $q, private templateSrv, private timeSrv) {
+    this.name = instanceSettings.name;
+    this.id = instanceSettings.id;
+    this.responseParser = new ResponseParser(this.$q);
+    this.interval = (instanceSettings.jsonData || {}).timeInterval;
+    this.queryModel = this.createQueryModel({});
+  }
+
+  abstract createQueryModel(target, templateSrv?, scopedVars?): SqlQuery;
+
+  interpolateVariable = (value, variable) => {
+    if (typeof value === 'string') {
+      if (variable.multi || variable.includeAll) {
+        return this.queryModel.quoteLiteral(value);
+      } else {
+        return value;
+      }
+    }
+
+    if (typeof value === 'number') {
+      return value;
+    }
+
+    const quotedValues = _.map(value, v => {
+      return this.queryModel.quoteLiteral(v);
+    });
+    return quotedValues.join(',');
+  };
+
+  query(options) {
+    const queries = _.filter(options.targets, target => {
+      return target.hide !== true;
+    }).map(target => {
+      const queryModel = this.createQueryModel(target, this.templateSrv, options.scopedVars);
+
+      return {
+        refId: target.refId,
+        intervalMs: options.intervalMs,
+        maxDataPoints: options.maxDataPoints,
+        datasourceId: this.id,
+        rawSql: queryModel.render(this.interpolateVariable),
+        format: target.format,
+      };
+    });
+
+    if (queries.length === 0) {
+      return this.$q.when({ data: [] });
+    }
+
+    return this.backendSrv
+      .datasourceRequest({
+        url: '/api/tsdb/query',
+        method: 'POST',
+        data: {
+          from: options.range.from.valueOf().toString(),
+          to: options.range.to.valueOf().toString(),
+          queries: queries,
+        },
+      })
+      .then(this.responseParser.processQueryResult);
+  }
+
+  annotationQuery(options) {
+    if (!options.annotation.rawQuery) {
+      return this.$q.reject({
+        message: 'Query missing in annotation definition',
+      });
+    }
+
+    const query = {
+      refId: options.annotation.name,
+      datasourceId: this.id,
+      rawSql: this.templateSrv.replace(options.annotation.rawQuery, options.scopedVars, this.interpolateVariable),
+      format: 'table',
+    };
+
+    return this.backendSrv
+      .datasourceRequest({
+        url: '/api/tsdb/query',
+        method: 'POST',
+        data: {
+          from: options.range.from.valueOf().toString(),
+          to: options.range.to.valueOf().toString(),
+          queries: [query],
+        },
+      })
+      .then(data => this.responseParser.transformAnnotationResponse(options, data));
+  }
+
+  metricFindQuery(query, optionalOptions) {
+    let refId = 'tempvar';
+    if (optionalOptions && optionalOptions.variable && optionalOptions.variable.name) {
+      refId = optionalOptions.variable.name;
+    }
+
+    const interpolatedQuery = {
+      refId: refId,
+      datasourceId: this.id,
+      rawSql: this.templateSrv.replace(query, {}, this.interpolateVariable),
+      format: 'table',
+    };
+
+    const range = this.timeSrv.timeRange();
+    const data = {
+      queries: [interpolatedQuery],
+      from: range.from.valueOf().toString(),
+      to: range.to.valueOf().toString(),
+    };
+
+    return this.backendSrv
+      .datasourceRequest({
+        url: '/api/tsdb/query',
+        method: 'POST',
+        data: data,
+      })
+      .then(data => this.responseParser.parseMetricFindQueryResult(refId, data));
+  }
+
+  testDatasource() {
+    return this.metricFindQuery('SELECT 1', {})
+      .then(res => {
+        return { status: 'success', message: 'Database Connection OK' };
+      })
+      .catch(err => {
+        console.log(err);
+        if (err.data && err.data.message) {
+          return { status: 'error', message: err.data.message };
+        } else {
+          return { status: 'error', message: err.status };
+        }
+      });
+  }
+}
