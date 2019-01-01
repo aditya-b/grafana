79dae9325ddd77938369ae3dc07021b76a365ffe
Sven Klemm <sven@timescale.com>
2019-01-01 14:07:00 +0100

    add SqlQueryCtrl and make mssql inherit from it

--- public/app/plugins/datasource/mssql/query_ctrl.ts
+++ public/app/plugins/datasource/mssql/query_ctrl.ts
@@ -1,16 +1,5 @@
 import _ from 'lodash';
-import { QueryCtrl } from 'app/plugins/sdk';
-
-export interface MssqlQuery {
-  refId: string;
-  format: string;
-  alias: string;
-  rawSql: string;
-}
-
-export interface QueryMeta {
-  sql: string;
-}
+import SqlQueryCtrl from 'app/plugins/datasource/sql/query_ctrl';
 
 const defaultQuery = `SELECT
   $__timeEpoch(<time_column>),
@@ -23,55 +12,9 @@ WHERE
 ORDER BY
   <time_column> ASC`;
 
-export class MssqlQueryCtrl extends QueryCtrl {
-  static templateUrl = 'partials/query.editor.html';
-
-  showLastQuerySQL: boolean;
-  formats: any[];
-  target: MssqlQuery;
-  lastQueryMeta: QueryMeta;
-  lastQueryError: string;
-  showHelp: boolean;
-
+export class MssqlQueryCtrl extends SqlQueryCtrl {
   /** @ngInject */
   constructor($scope, $injector) {
-    super($scope, $injector);
-
-    this.target.format = this.target.format || 'time_series';
-    this.target.alias = '';
-    this.formats = [{ text: 'Time series', value: 'time_series' }, { text: 'Table', value: 'table' }];
-
-    if (!this.target.rawSql) {
-      // special handling when in table panel
-      if (this.panelCtrl.panel.type === 'table') {
-        this.target.format = 'table';
-        this.target.rawSql = 'SELECT 1';
-      } else {
-        this.target.rawSql = defaultQuery;
-      }
-    }
-
-    this.panelCtrl.events.on('data-received', this.onDataReceived.bind(this), $scope);
-    this.panelCtrl.events.on('data-error', this.onDataError.bind(this), $scope);
-  }
-
-  onDataReceived(dataList) {
-    this.lastQueryMeta = null;
-    this.lastQueryError = null;
-
-    const anySeriesFromQuery = _.find(dataList, { refId: this.target.refId });
-    if (anySeriesFromQuery) {
-      this.lastQueryMeta = anySeriesFromQuery.meta;
-    }
-  }
-
-  onDataError(err) {
-    if (err.data && err.data.results) {
-      const queryRes = err.data.results[this.target.refId];
-      if (queryRes) {
-        this.lastQueryMeta = queryRes.meta;
-        this.lastQueryError = queryRes.error;
-      }
-    }
+    super($scope, $injector, defaultQuery);
   }
 }
