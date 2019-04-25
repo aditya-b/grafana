8c63272b99edfe9d7d2d12fb10fd81ec87267f26
Torkel Ödegaard <torkel@grafana.com>
2019-04-25 11:27:23 +0200

    jest performance testing

---
+++ public/app/plugins/datasource/elasticsearch/specs/muppo.test.ts
@@ -0,0 +1,10 @@
+import { getFlotPairs } from '@grafana/ui';
+
+describe('mupppo', () => {
+  it('should work', () => {
+    expect(true).toEqual(true);
+  });
+  it('should work2', () => {
+    expect(getFlotPairs).toEqual(false);
+  });
+});
