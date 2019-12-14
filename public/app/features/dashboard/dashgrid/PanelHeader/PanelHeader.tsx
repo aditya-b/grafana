6078ca230886865facf2c503a86f8e9397465fcb
Torkel Ödegaard <torkel@grafana.com>
2019-12-14 22:18:12 +0100

    new panel header poc

--- public/app/features/dashboard/dashgrid/PanelHeader/PanelHeader.tsx
+++ public/app/features/dashboard/dashgrid/PanelHeader/PanelHeader.tsx
@@ -1,17 +1,58 @@
+// Libraries
 import React, { Component } from 'react';
-import classNames from 'classnames';
 import { isEqual } from 'lodash';
-import { DataLink, ScopedVars } from '@grafana/data';
-import { ClickOutsideWrapper } from '@grafana/ui';
-import { e2e } from '@grafana/e2e';
+import { css, cx } from 'emotion';
 
-import PanelHeaderCorner from './PanelHeaderCorner';
-import { PanelHeaderMenu } from './PanelHeaderMenu';
+// Utils & Services
+import config from 'app/core/config';
+import { stylesFactory } from '@grafana/ui';
 import templateSrv from 'app/features/templating/template_srv';
+/* import { getPanelLinksSupplier } from 'app/features/panel/panellinks/linkSuppliers'; */
+/* import { e2e } from '@grafana/e2e'; */
 
+// Components
+import { ClickOutsideWrapper } from '@grafana/ui';
+/* import PanelHeaderCorner from './PanelHeaderCorner'; */
+import { PanelHeaderMenu } from './PanelHeaderMenu';
+
+// Types
+import { GrafanaTheme } from '@grafana/data';
+import { DataLink, ScopedVars } from '@grafana/data';
 import { DashboardModel } from 'app/features/dashboard/state/DashboardModel';
 import { PanelModel } from 'app/features/dashboard/state/PanelModel';
-import { getPanelLinksSupplier } from 'app/features/panel/panellinks/linkSuppliers';
+
+const getStyles = stylesFactory((theme: GrafanaTheme) => ({
+  header: css`
+    width: 100%;
+    display: flex;
+    flex-wrap: nowrap;
+    cursor: move;
+    padding: ${theme.spacing.sm} ${theme.spacing.sm} 0;
+    font-size: 14px;
+  `,
+  titleContainer: css`
+    flex-grow: 0;
+  `,
+  title: css`
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    font-weight: 500;
+  `,
+  stateIconContainer: css`
+    display: flex;
+    padding-left: ${theme.spacing.xs};
+    flex-grow: 1;
+  `,
+  stateIcon: css`
+    width: 16px;
+    height: 16px;
+  `,
+  menuIcon: css`
+    width: 16px;
+    height: 16px;
+  `,
+}));
 
 export interface Props {
   panel: PanelModel;
@@ -72,48 +113,46 @@ export class PanelHeader extends Component<Props, State> {
     });
   };
 
+  /* <PanelHeaderCorner */
+  /*           panel={panel} */
+  /*           title={panel.title} */
+  /*           description={panel.description} */
+  /*           scopedVars={panel.scopedVars} */
+  /*           links={getPanelLinksSupplier(panel)} */
+  /*           error={error} */
+  /*         /> */
+  /* onClick={this.onMenuToggle} */
+  /* onMouseDown={this.onMouseDown} */
+  /* aria-label={e2e.pages.Panels.Panel.selectors.title(title)} */
+
+  /* {timeInfo && ( */
+  /*                 <span className="panel-time-info"> */
+  /*                   <i className="fa fa-clock-o" /> {timeInfo} */
+  /*                 </span> */
+  /*               )} */
+
   render() {
     const { panel, dashboard, timeInfo, scopedVars, error, isFullscreen } = this.props;
     const title = templateSrv.replaceWithText(panel.title, scopedVars);
-
-    const panelHeaderClass = classNames({
-      'panel-header': true,
-      'grid-drag-handle': !isFullscreen,
-    });
+    const styles = getStyles(config.theme);
 
     return (
       <>
-        <div className={panelHeaderClass}>
-          <PanelHeaderCorner
-            panel={panel}
-            title={panel.title}
-            description={panel.description}
-            scopedVars={panel.scopedVars}
-            links={getPanelLinksSupplier(panel)}
-            error={error}
-          />
-          <div
-            className="panel-title-container"
-            onClick={this.onMenuToggle}
-            onMouseDown={this.onMouseDown}
-            aria-label={e2e.pages.Panels.Panel.selectors.title(title)}
-          >
-            <div className="panel-title">
-              <span className="icon-gf panel-alert-icon" />
-              <span className="panel-title-text">
-                {title} <span className="fa fa-caret-down panel-menu-toggle" />
-              </span>
-              {this.state.panelMenuOpen && (
-                <ClickOutsideWrapper onClick={this.closeMenu}>
-                  <PanelHeaderMenu panel={panel} dashboard={dashboard} />
-                </ClickOutsideWrapper>
-              )}
-              {timeInfo && (
-                <span className="panel-time-info">
-                  <i className="fa fa-clock-o" /> {timeInfo}
-                </span>
-              )}
-            </div>
+        <div className={cx(styles.header, { 'grid-drag-handle': !isFullscreen })}>
+          <div className={styles.titleContainer}>
+            <div className={styles.title}>{title}</div>
+          </div>
+          <div className={styles.stateIconContainer}>
+            <i className={cx(styles.stateIcon, 'fa fa-info')} />
+            <i className={cx(styles.stateIcon, 'fa fa-warning')} />
+          </div>
+          <div className={styles.menuIcon}>
+            <i className={cx('fa fa-bars')} onClick={this.onMenuToggle} />
+            {this.state.panelMenuOpen && (
+              <ClickOutsideWrapper onClick={this.closeMenu}>
+                <PanelHeaderMenu panel={panel} dashboard={dashboard} />
+              </ClickOutsideWrapper>
+            )}
           </div>
         </div>
       </>
